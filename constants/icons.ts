d721d90454ed1637d7b4c970da8404b40def5a98
fanyicharllson <fanyicharllson1@gmail.com>
2025-09-23 05:58:52 -0700

    Add Prettier configuration and implement tab navigation with screens for Home, Search, Saved, and Profile

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,4 +1,4 @@
-import home from "@/assets/icons/home.png";
+import home from "../assets/icons/home.png";
 import search from "@/assets/icons/search.png";
 import person from "@/assets/icons/person.png";
 import logo from "@/assets/icons/logo.png";
